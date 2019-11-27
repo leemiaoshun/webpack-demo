66cf826cd2dedc83ece44719244513723fcc5ed8
miaoshun.li <miaoshun.li@verystar.net>
2019-11-27 18:07:10 +0800

    feat:新增字体loader处理

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import _ from 'lodash';
 import './style.css';
+import './font/iconfont.css'
+
 import myImg from './img/fehelper-www-webpackjs-com-guides-asset-management-1574845141553.png'
 
 function component() {
   var element = document.createElement('div');
+  var span = document.createElement('span');
+  span.classList.add('iconfont');
+  span.classList.add('icon');
+  span.classList.add('icon-refresh');
 
   // lodash 是由当前 script 脚本 import 导入进来的
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
@@ -13,6 +19,7 @@ function component() {
   const myIcon = new Image();
   myIcon.src = myImg;
   element.appendChild(myIcon);
+  element.appendChild(span);
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,12 @@ module.exports = {
     }, {
       test: /\.(png|jpg|svg|gif)$/,
       use: ['file-loader']
+    },
+    {
+      test: /\.(woff|woff2|eot|ttf|otf)$/,
+      use: [
+        'file-loader'
+      ]
     }]
   }
 }
